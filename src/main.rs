226f21df33a18e3ac6978c0120efc2b18803f86b
Bruno De Masi <bruno@demasi.dev>
2023-10-22 03:31:29 -0300

    feat: tokio and axum for api endpoint supports

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
-use std::io;
+use std::{error::Error, net::SocketAddr};
 mod db;
 
-fn main() -> io::Result<()> {
+use axum::{
+    routing::get,
+    Router
+};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+    let app = Router::new().route("/", get(|| async {"Hello, Axum"}));
+
+    println!("listening on {}", addr);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+
     Ok(())
 }
